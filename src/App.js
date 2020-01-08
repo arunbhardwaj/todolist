a8dff898eeffa5e3dd7d3a30ed3736d22e481e32
arunbhardwaj <arun.b.95@gmail.com>
2020-01-08 17:00:12 -0500

    Adds functionality and component drilling for each todoItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-          <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'take out the',
+                completed: false
+            },
+            {
+                id: 3,
+                title: 'take out trash',
+                completed: false
+            }
+        ]
+    }
+
+    markComplete = (e) => {
+        
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {/* we take the state in app and assign it as a property to the Todos component where we can work with it further */}
+                <Todos todos={this.state.todos} markComplete={this.markComplete} />
+            </div>
+        );
+    }
 }
 
 export default App;
