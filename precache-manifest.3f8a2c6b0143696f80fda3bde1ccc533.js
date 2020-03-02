2b2054355637730ec7eae5c4f8ddf222f48c8108
arunbhardwaj <arun.b.95@gmail.com>
2020-03-01 23:57:35 -0500

    Updates

---
+++ precache-manifest.3f8a2c6b0143696f80fda3bde1ccc533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96050eede27dae851252a56ab5734aa",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "37ce43b9051418659023",
+    "url": "/todolist/static/js/2.a7ac95bc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todolist/static/js/2.a7ac95bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56bbafdae1f51dadcc7e",
+    "url": "/todolist/static/js/main.e6150c01.chunk.js"
+  },
+  {
+    "revision": "507716673e279f58353e",
+    "url": "/todolist/static/js/runtime-main.948c6c71.js"
+  }
+]);
